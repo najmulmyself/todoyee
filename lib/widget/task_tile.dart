0a56e758903b7c2a56ce901cb9947a41f0d2c97a
najmulmyself <najmul.myself@gmail.com>
2022-09-07 12:30:00 +0600

    managed to get setS within checkbox and text

--- lib/widget/task_tile.dart
+++ lib/widget/task_tile.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   const TaskTile({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task'),
+      title: Text(
+        'This is a task',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
+      ),
       trailing: Checkbox(
-        value: false,
-        onChanged: (value) {},
+        activeColor: Colors.blueAccent,
+        value: isChecked,
+        onChanged: (value) {
+          // THIS VALUE WILL CHANGE EVERYTIME WHEN USER CLICK ON BUTTON
+          setState(() {
+            isChecked = value!;
+          });
+        },
       ),
     );
   }
